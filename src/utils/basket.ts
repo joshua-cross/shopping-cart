93e0368825e029f5e124bd8a372f6e3ccebc1c45
joshua-cross <crossjoshua6@gmail.com>
2019-12-19 15:09:20 +0000

    Created a small shopping cart application using TDD that allows us to add, remove items from a shopping cart

---
+++ src/utils/basket.ts
@@ -0,0 +1,160 @@
+export default class Basket {
+    private totalPrice: number = 0;
+
+    private items: object = {
+        'apple': {
+            price: 0.50,
+            quantity: 0,
+            discount: this.defaultCalculator,
+            initialStock: 3,
+            stock: 3
+        },
+        'orange': {
+            price: 0.75,
+            quantity: 0,
+            discount: this.bogofCalculator,
+            initialStock: 10,
+            stock: 10
+        },
+        'pear': {
+            price: 1,
+            quantity: 0,
+            discount: this.threeForTwoCalculator,
+            initialStock: 5,
+            stock: 5
+        }
+    }
+
+    private percentageOffCalculator(price: number, quantity: number, discountAmount: number): number {
+        if(discountAmount !== 0) {
+            let itemTotal: number = +(quantity * price * (1 - discountAmount / 100)).toFixed(2);
+            return itemTotal;
+        } else {
+            return price * quantity;
+        }
+    }
+
+    private threeForTwoCalculator(price: number, quantity: number): number {
+        if(quantity > 0) {
+            let timesToApply: number = Math.floor(quantity / 3);
+            console.log(`${quantity} pears's should apply discount ${timesToApply} times`);
+            return price * quantity - (timesToApply * price);
+        } else {
+            return quantity * price;
+        }
+    }
+
+    private bogofCalculator(price: number, quantity: number): number {
+        if(quantity > 0) {
+            let timesToApply: number = Math.floor(quantity / 2);
+            console.log(`${quantity} orange's should apply discount ${timesToApply} times`);
+            return price * quantity - (timesToApply * price);
+        } else {
+            return quantity * price;
+        }
+    }
+
+    private defaultCalculator(price: number, quantity: number): number {
+        return price * quantity;
+    }
+    
+    private discounts: object = {
+        'apple': this.defaultCalculator,
+        'orange': this.bogofCalculator,
+        'pear': this.threeForTwoCalculator
+    }
+
+    private itemQuantities: object = {
+        'apple': 0,
+        'orange': 0,
+        'pear': 0
+    }
+
+    private itemPrices: object = {
+        'apple': 0.50,
+        'orange': 0.75,
+        'pear': 1
+    }
+
+    public setTotalPrice(cost: number): void {
+        this.totalPrice += cost;
+    } 
+
+    public getTotalPrice(): number {
+        return this.totalPrice;
+    }
+
+    public scan(item: string): void {
+        if(Object(this.items)[item] !== undefined) {
+            if(Object(this.items)[item].stock > 0) {
+                Object(this.items)[item].quantity += 1;
+                Object(this.items)[item].stock -= 1;
+            }
+        }
+    }
+
+    public remove(item: string): void {
+        if(Object(this.items)[item] !== undefined) {
+            if(Object(this.items)[item].quantity > 0) {
+                Object(this.items)[item].quantity -= 1;
+                Object(this.items)[item].stock += 1;
+            }
+        }
+    }
+
+    public addItem(name: string, price: number, stock: number, discount: string = "", percentageOff: number = 0): void {
+
+        var functionToUse;
+        if(discount === "3 for 2") {
+            functionToUse = this.threeForTwoCalculator;
+        } else if (discount === "bogof") {
+            functionToUse = this.bogofCalculator;
+        } else if (discount === "percent off") {
+            functionToUse = this.percentageOffCalculator;
+        } else {
+            functionToUse = this.defaultCalculator;
+        }
+        
+        var newItem: object = {
+
+        }
+
+        Object(newItem)[name] = {
+            price: price,
+            quantity: 0,
+            discount: functionToUse,
+            percentageOff: percentageOff,
+            stock: stock,
+            initialStock: stock
+        }
+
+        this.items = {...this.items, ...newItem};
+        // console.log(this.items);
+    }
+
+    public total(): number {
+        this.totalPrice = 0;
+
+        for(let key in this.items) {
+            const ITEM = Object(this.items)[key];
+            this.totalPrice += ITEM.discount(ITEM.price, ITEM.quantity, ITEM.percentageOff);
+        }
+        return this.getTotalPrice();
+    } 
+
+    public getStock(item: string): number {
+        return Object(this.items)[item].stock;
+    }
+
+    public getQuantity(item: string): number {
+        return Object(this.items)[item].quantity;
+    }
+
+    public cancel(): void {
+        for(let key in this.items) {
+            const ITEM = Object(this.items)[key];
+            ITEM.stock = ITEM.initialStock;
+            ITEM.quantity = 0;
+        }
+    }
+}
---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,207 @@
+import { shallowMount } from '@vue/test-utils'
+import Basket from '@/utils/basket';
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+
+describe('Basket', () => {
+  it('Adds the total of a single apple', () => {
+    let basket: Basket = new Basket();
+    basket.scan("apple");
+    expect(basket.total()).toEqual(0.5);
+  })
+
+  it('Adds the total of 2 apples', () => {
+    let basket: Basket = new Basket();
+    basket.scan("apple");
+    basket.scan("apple");
+    expect(basket.total()).toEqual(1);
+  })
+
+  it('Expects an unidentified item to return 0', () => {
+    let basket: Basket = new Basket();
+    basket.scan('watermelon');
+    expect(basket.total()).toEqual(0);
+  })
+
+  it('Expects 1 orange to cost 75p', () => {
+    let basket: Basket = new Basket();
+    basket.scan('orange');
+    expect(basket.total()).toEqual(0.75);
+  })
+  
+  it('Expects 2 oranges to cost 75p (BOGOF)', () => {
+    let basket: Basket = new Basket();
+    basket.scan('orange');
+    basket.scan('orange');
+    expect(basket.total()).toEqual(0.75);
+  })
+
+  it('Expects 3 oranges to cost £1.50 (BOGOF)', () => {
+    let basket: Basket = new Basket();
+    basket.scan('orange');
+    basket.scan('orange');
+    basket.scan('orange');
+    expect(basket.total()).toEqual(1.50);
+  })
+  
+  it('Expects 1 pear to cost £1', () => {
+    let basket: Basket = new Basket();
+    basket.scan('pear');
+    expect(basket.total()).toEqual(1);
+  })
+
+  it('Expects 2 pears to cost £2', () => {
+    let basket: Basket = new Basket();
+    basket.scan('pear');
+    basket.scan('pear');
+    expect(basket.total()).toEqual(2);
+  })
+
+  it('Expects 3 pears to equal £2 (3 for 2)', () => {
+    let basket: Basket = new Basket();
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    expect(basket.total()).toEqual(2);
+  })
+
+  it('Should reduce apple to 50p when 1 is reduced.', () => {
+    let basket: Basket = new Basket();
+    basket.scan('apple');
+    basket.scan('apple');
+    basket.total();
+    basket.remove('apple')
+    expect(basket.total()).toEqual(0.50);
+  });
+
+  it('Should remove the 3 for 2 discount if we remove a pear', () => {
+    let basket: Basket = new Basket();
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.total();
+    basket.remove('pear');
+    expect(basket.total()).toEqual(2)
+  })
+  it('Should not affect the price when an invalid item is removed', () => {
+    let basket: Basket = new Basket();
+    basket.scan('apple');
+    basket.total();
+    basket.remove('watermelon')
+    expect(basket.total()).toEqual(0.50);
+  });
+
+  it('Should decrease stock when item is scanned (expected 2 apples)', () => {
+    let basket: Basket = new Basket();
+    basket.scan('apple');
+    expect(basket.getStock('apple')).toEqual(2);
+  })
+
+  it('Should remain 3 apples in stock if we scan and remove item', () => {
+    let basket: Basket = new Basket();
+    basket.scan('apple');
+    basket.remove('apple')
+    expect(basket.getStock('apple')).toEqual(3);
+  })
+
+  it('should not allow users to remove stock if unavailable', () => {
+    let basket: Basket = new Basket();
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    basket.scan('pear');
+    expect(basket.getStock('pear')).toEqual(0);
+    expect(basket.total()).toEqual(4);
+  })
+
+  it('should return all stock to inventory when canceled', () => {
+    let basket: Basket = new Basket();
+    basket.scan('apple');
+    basket.scan('apple');
+    basket.scan('orange');
+    basket.scan('orange');
+    basket.scan('orange');
+    basket.scan('pear');
+    basket.cancel();
+    expect(basket.getStock('apple')).toEqual(3)
+    expect(basket.getStock('orange')).toEqual(10);
+    expect(basket.getStock('pear')).toEqual(5);
+  })
+
+  it('should set total to 0 when canceled as no items are in the basket.', () => {
+    let basket: Basket = new Basket();
+    basket.scan('apple')
+    basket.scan('apple')
+    basket.scan('apple');
+    basket.cancel();
+    expect(basket.total()).toEqual(0);
+  });
+
+  it('should not let us remove an item if the quantitity is 0', () => {
+    let basket: Basket = new Basket();
+    basket.remove('apple');
+    expect(basket.getQuantity('apple')).toEqual(0);
+  })
+
+  it('should add watermelon and should equal £1.50', () => {
+    let basket: Basket = new Basket();
+    basket.addItem('watermelon', 1.50, 5);
+    basket.scan('watermelon');
+    expect(basket.total()).toEqual(1.50);
+  })
+
+  it('should add watermelon and apply 3 for 2 discount and should equal £1.50', () => {
+    let basket: Basket = new Basket();
+    basket.addItem('watermelon', 1.50, 5, "3 for 2");
+    basket.scan('watermelon');
+    basket.scan('watermelon');
+    basket.scan('watermelon');
+    expect(basket.total()).toEqual(3);
+  })
+
+  it('should add watermelon and apply 3 for 2 discount and should equal £1.50', () => {
+    let basket: Basket = new Basket();
+    basket.addItem('watermelon', 1.50, 5, "bogof");
+    basket.scan('watermelon');
+    basket.scan('watermelon');
+    expect(basket.total()).toEqual(1.50);
+  });
+
+  it('should remove 10% off the cost of a £1.50 water melon (£1.35)', () => {
+    let basket: Basket = new Basket();
+    basket.addItem('watermelon', 1.50, 5, "percent off", 10);
+    basket.scan('watermelon');
+    expect(basket.total()).toEqual(1.35);
+  })
+
+  it('Should return £1.50 if we attempt to apply a 0% discount', () => {
+    let basket: Basket = new Basket();
+    basket.addItem('watermelon', 1.50, 5, "percent off", 0);
+    basket.scan('watermelon');
+    expect(basket.total()).toEqual(1.50);
+  })
+
+  it('Should return £1.65 if we attempt to apply a -10% discount', () => {
+    let basket: Basket = new Basket();
+    basket.addItem('watermelon', 1.50, 5, "percent off", -10);
+    basket.scan('watermelon');
+    expect(basket.total()).toEqual(1.65);
+  })
+
+  
+});
